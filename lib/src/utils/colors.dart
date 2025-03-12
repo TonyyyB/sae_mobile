d98f242aa9c2e3d219b019eefab5bedf156965e5
Tony <tony.beaujouan@etu.univ-orleans.fr>
2025-03-12 12:18:46 +0100

    Ajout du theme pour la police

--- lib/src/utils/colors.dart
+++ lib/src/utils/colors.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 class PickMenuColors{
+  static const Color textColor = Color(0xFF3d6247);
   static const Color backgroundColor = Color(0xFFFFFFFF);
   static const Color borderColor = Color(0xFF75A482);
 
   static const Color textFieldErrorBorder = Color(0xFFE57373);
+
+  static const Color buttonColor = Color(0xFF75A482);
 }
