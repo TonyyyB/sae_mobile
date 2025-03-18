0e47eefdd1ea01e601a26539164424761956bf59
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-03-18 16:58:59 +0100

    début méthodes restaurant

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -1,3 +1,5 @@
+import 'avis.dart';
+
 class Restaurant {
   String osmId;
   double longitude;
@@ -21,13 +23,26 @@ class Restaurant {
   String region;
   String departement;
   String commune;
-  List<String>? avis;
+  List<Avis>? avis;
 
   Restaurant({required this.osmId,required this.longitude,required this.latitude,required
   this.type,required this.name , this.operator, this.brand, this.openingHours, this.wheelchair,
   required this.cuisine, this.vegetarian, this.vegan, this.delivery, this.takeaway, this.capacity,
   this.driveThrough, this.phone, this.website, this.facebook, required this.region, required this.departement,
   required this.commune, this.avis});
+
+  set setAvis(List<Avis> listeAvis){
+    this.avis = listeAvis;
+  }
+
+  List<Avis>? get getAvis{
+    return this.avis;
+  }
+
+  addAvis(Avis avis){
+    this.avis ??= [];
+    this.avis?.add(avis);
+  }
 
   //static List<Task> generateTask(int i){
    // List<Task> tasks=[];
