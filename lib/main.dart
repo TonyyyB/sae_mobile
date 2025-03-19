405a9434ab990c0f09ed6202d7c14e4646328ca0
Tony <tony.beaujouan@etu.univ-orleans.fr>
2025-03-19 11:51:17 +0100

    Première implémentation du système de signin et de signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:sae_mobile/config/theme.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'config/router.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+      url: dotenv.env['DB_URL']!, anonKey: dotenv.env['DB_KEY']!);
   runApp(MyApp());
 }
 
