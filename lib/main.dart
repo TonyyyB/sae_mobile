30465c16c1feecde9242f21b9e792c1bb6f72e8a
Tony <tony.beaujouan@etu.univ-orleans.fr>
2025-03-19 11:29:47 +0100

    Ajout du début de la base de donnée. Attention, il faut créer un fichier .env dans assets/ avec les variables DB_URL et DB_KEY

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:sae_mobile/config/theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'config/router.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
 
