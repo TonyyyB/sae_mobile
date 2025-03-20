e6ec61a7158b2054bdd595b0130a1641cc168fcb
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-03-20 15:03:02 +0100

    ajout des images dans les avis

--- lib/models/avis.dart
+++ lib/models/avis.dart
@@ -2,8 +2,9 @@ class Avis{
   String utilisateur;
   String? commentaire;
   int note;
+  String? image;
 
-  Avis({required this.utilisateur, this.commentaire, required this.note});
+  Avis({required this.utilisateur, this.commentaire, required this.note, this.image});
 
   int get getNote{
     return this.note;
@@ -26,6 +27,18 @@ class Avis{
       this.commentaire = null;
     } else {
       this.commentaire = newComm;
+    }
+  }
+
+  String? get getImage{
+    return this.image;
+  }
+
+  set setImage(String newImg){
+    if (newImg.isEmpty){
+      this.image = null;
+    } else {
+      this.image = newImg;
     }
   }
 }
