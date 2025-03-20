2eced0d4fd32680dcd30787b31a39ebeacfb304a
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-03-20 12:00:14 +0100

    Classe avis, correction restaurant et suppression fichier bizarre

--- lib/models/avis.dart
+++ lib/models/avis.dart
@@ -1,3 +1,31 @@
 class Avis{
+  String utilisateur;
+  String? commentaire;
+  int note;
 
+  Avis({required this.utilisateur, this.commentaire, required this.note});
+
+  int get getNote{
+    return this.note;
+  }
+
+  String get getUtilisateur{
+    return this.utilisateur;
+  }
+
+  String? get getCommentaire{
+    return this.commentaire;
+  }
+
+  set setNote(int newNote){
+    this.note = newNote;
+  }
+
+  set setCommentaire(String newComm){
+    if (newComm.isEmpty){
+      this.commentaire = null;
+    } else {
+      this.commentaire = newComm;
+    }
+  }
 }
