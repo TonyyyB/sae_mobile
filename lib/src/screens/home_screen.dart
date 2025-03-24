de0eb74747c6a355bdb77b5769ee031a8cfd7dd5
varuniv <sargis.vardanyan@etu.univ-orelans.fr>
2025-03-24 18:05:09 +0100

    ajout de class noteetoile

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:sae_mobile/src/widgets/scaffold.dart';
 
+import '../../config/colors.dart';
+import '../widgets/noteEtoile.dart';
+
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return PickMenuScaffold(
-      child: const Text("Coucou"),
+      child: Row(
+        children: [
+          NoteEtoile(rating: 2.00001)
+        ],
+      )
+
+
+
     );
   }
 }
---
+++ lib/src/widgets/noteEtoile.dart
@@ -0,0 +1,68 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:sae_mobile/src/widgets/scaffold.dart';
+import '../../config/colors.dart';
+
+ const Icon star = Icon(color:PickMenuColors.iconsColor, IconData(0xe5f9, fontFamily: 'MaterialIcons'));
+ const Icon star_empty = Icon(color:PickMenuColors.iconsColor,IconData(0xe5fa, fontFamily: 'MaterialIcons'));
+
+class SingleRatingIcon extends StatelessWidget {
+  final IconData icon;
+  final double size;
+  final Color iconColor;
+  final double rating;
+
+  const SingleRatingIcon({super.key, required this.icon, required this.size, required this.iconColor, required this.rating});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      blendMode: BlendMode.srcATop,
+      shaderCallback: (Rect rect) {
+        return LinearGradient(
+          stops: [0, rating / 1, rating / 1],
+          colors: [iconColor, iconColor, iconColor.withOpacity(0)],
+        ).createShader(rect);
+      },
+      child: SizedBox(
+        width: size,
+        height: size,
+        child: Icon(icon, size: size, color: Colors.grey[300]),
+      ),
+    );
+  }
+}
+
+class NoteEtoile extends StatelessWidget{
+
+  double size ;
+  Color iconColor ;
+  final double rating;
+
+  NoteEtoile({super.key,  this.size = 20, this.iconColor = PickMenuColors.iconsColor, required this.rating});
+  @override
+  Widget build(BuildContext context){
+    double cpt = rating;
+    Row res = Row(
+        children: <Widget>[]
+    );
+    for (var i = 0; i < 5; i++) {
+      if (cpt>=1){
+      res.children.add(star);
+      cpt--;
+      }else if(cpt>0) {
+        res.children.add(SingleRatingIcon(icon: Icons.star, size: size, iconColor: PickMenuColors.iconsColor, rating: cpt));
+        cpt = 0;
+      }else if (cpt==0){
+        res.children.add(star_empty);
+      }
+
+    }
+
+    return res;
+  }
+}
+
+
