45ef366b10ad588091389c3c14d4475c4d6b7a77
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-03-25 16:59:06 +0100

    création du bouton titre

--- lib/src/widgets/restaurant_card.dart
+++ lib/src/widgets/restaurant_card.dart
@@ -1,3 +1,6 @@
+import 'dart:nativewrappers/_internal/vm/lib/internal_patch.dart';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sae_mobile/src/widgets/noteEtoile.dart';
 import '../../config/colors.dart';
