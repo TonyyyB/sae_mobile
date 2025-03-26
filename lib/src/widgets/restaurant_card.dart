b98226729e4aeddd187b1e457d198cc6638a5b89
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-03-26 10:59:13 +0100

    carte entièrement cliquable

--- lib/src/widgets/restaurant_card.dart
+++ lib/src/widgets/restaurant_card.dart
@@ -1,6 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/internal_patch.dart';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../../config/colors.dart';
 
@@ -10,36 +7,39 @@ class RestaurantCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+            elevation: 10,
             clipBehavior: Clip.antiAlias,
             margin : EdgeInsets.all(50.0),
-            child: Column(
-              children: [
-                Container(
-                  decoration: const BoxDecoration(
-                    border : Border(
-                      bottom: BorderSide(width: 2.0, color: PickMenuColors.textColor),
-                    ),
+            child: GestureDetector(
+            onTap : (){ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Click')));},
+            child :Column(
+              children: [ Container(
+                decoration: const BoxDecoration(
+                  border : Border(
+                    bottom: BorderSide(width: 2.0, color: PickMenuColors.textColor),
                   ),
-                  margin : EdgeInsets.fromLTRB(40, 20, 40, 20),
-                  child: Padding(
-                      padding : const EdgeInsets.fromLTRB(0, 0, 0, 20),
-                      child : Row(
-                        children :[TextButton(onPressed: () {}, child: const Text('Nom du restau', style: TextStyle(color: PickMenuColors.textColor, fontSize: 24)))
-                          ]
-                      ),
+                ),
+                margin : EdgeInsets.fromLTRB(40, 20, 40, 20),
+                child: Padding(
+                  padding : const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                  child : Row(
+                      children :[const Text('Nom du restau', style: TextStyle(color: PickMenuColors.textColor, fontSize: 24))
+                      ]
                   ),
-                  ),
+                ),
+              ),
+
                 const Padding(
                   padding: EdgeInsets.all(16),
                   child: Text(
-                    'Les horaires',
-                    style: TextStyle(color: PickMenuColors.inputHint),
+                    'Mo : 8h-12h 14h-18h\nMo : 8h-12h 14h-18h\nMo : 8h-12h 14h-18h\nMo : 8h-12h 14h-18h\nMo : 8h-12h 14h-18h\n',
+                    style: TextStyle(color: PickMenuColors.inputHint, fontSize : 16),
                   ),
                 ),
-
               ],
             ),
-          );
+          ));
   }
 }
 
