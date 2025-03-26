b7a08024a57e8e5c7dd430a365a05347758c3d85
Tony <tony.beaujouan@etu.univ-orleans.fr>
2025-03-26 18:02:24 +0100

    Modification du model

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -1,139 +1,117 @@
 import 'avis.dart';
 
 class Restaurant {
-  static Map<String, int> typesCuisine = {};
-  String _osmId;
-  double _longitude;
-  double _latitude;
-  String _type;
-  String _name;
-  String? _operator;
-  String? _brand;
-  List<String>? _openingHours;
-  bool? _wheelchair;
+  final int _osmId;
+  final double _longitude;
+  final double _latitude;
+  final String _type;
+  final String _name;
+  final String? _operator;
+  final String? _brand;
+  final List<String>? _openingHours;
+  final bool? _wheelchair;
   List<String>? _cuisine;
-  bool? _vegetarian;
-  bool? _vegan;
-  bool? _delivery;
-  bool? _takeaway;
-  String? _capacity;
-  bool? _driveThrough;
-  String? _phone;
-  String? _website;
-  String? _facebook;
-  String _region;
-  String _departement;
-  String _commune;
+  final bool? _vegetarian;
+  final bool? _vegan;
+  final bool? _delivery;
+  final bool? _takeaway;
+  final String? _capacity;
+  final bool? _driveThrough;
+  final String? _phone;
+  final String? _website;
+  final String? _facebook;
+  final String _region;
+  final String _departement;
+  final String _commune;
   List<Avis>? _avis;
 
-  Restaurant({required String osmId,required double longitude,required double latitude,required
-  String type,required String name , String? operator, String? brand, List<String>? openingHours, bool? wheelchair,
-  List<String>? cuisine, bool? vegetarian, bool? vegan, bool? delivery, bool? takeaway, String? capacity,
-  bool? driveThrough, String? phone, String? website, String? facebook, required String region, required String departement,
-  required String commune, List<Avis>? avis}) : _avis = avis, _commune = commune, _departement = departement, _region = region, _facebook = facebook, _website = website, _phone = phone, _driveThrough = driveThrough, _capacity = capacity, _takeaway = takeaway, _delivery = delivery, _vegan = vegan, _vegetarian = vegetarian, _cuisine = cuisine, _wheelchair = wheelchair, _openingHours = openingHours, _brand = brand, _operator = operator, _name = name, _type = type, _latitude = latitude, _longitude = longitude, _osmId = osmId;
+  Restaurant(
+      {required int osmId,
+      required double longitude,
+      required double latitude,
+      required String type,
+      required String name,
+      String? operator,
+      String? brand,
+      List<String>? openingHours,
+      bool? wheelchair,
+      List<String>? cuisine,
+      bool? vegetarian,
+      bool? vegan,
+      bool? delivery,
+      bool? takeaway,
+      String? capacity,
+      bool? driveThrough,
+      String? phone,
+      String? website,
+      String? facebook,
+      required String region,
+      required String departement,
+      required String commune,
+      List<Avis>? avis})
+      : _avis = avis,
+        _commune = commune,
+        _departement = departement,
+        _region = region,
+        _facebook = facebook,
+        _website = website,
+        _phone = phone,
+        _driveThrough = driveThrough,
+        _capacity = capacity,
+        _takeaway = takeaway,
+        _delivery = delivery,
+        _vegan = vegan,
+        _vegetarian = vegetarian,
+        _cuisine = cuisine,
+        _wheelchair = wheelchair,
+        _openingHours = openingHours,
+        _brand = brand,
+        _operator = operator,
+        _name = name,
+        _type = type,
+        _latitude = latitude,
+        _longitude = longitude,
+        _osmId = osmId;
 
-  set setAvis(List<Avis> listeAvis){
-    this._avis = listeAvis;
+  int get osmId => _osmId;
+  double get longitude => _longitude;
+  double get latitude => _latitude;
+  String get type => _type;
+  String get name => _name;
+  String? get operator => _operator;
+  String? get brand => _brand;
+  List<String>? get openingHours => _openingHours;
+  bool? get wheelchair => _wheelchair;
+  List<String>? get cuisine => _cuisine;
+  bool? get vegetarian => _vegetarian;
+  bool? get vegan => _vegan;
+  bool? get delivery => _delivery;
+  bool? get takeaway => _takeaway;
+  String? get capacity => _capacity;
+  bool? get driveThrough => _driveThrough;
+  String? get phone => _phone;
+  String? get website => _website;
+  String? get facebook => _facebook;
+  String get region => _region;
+  String get departement => _departement;
+  String get commune => _commune;
+  List<Avis>? get avis => _avis;
+
+  set cuisine(List<String>? value) => _cuisine = value;
+  set avis(List<Avis>? value) => _avis = value;
+
+  void addAvis(Avis avis) {
+    _avis ??= [];
+    _avis!.add(avis);
   }
 
-  List<Avis>? get getAvis{
-    return this._avis;
+  void addCuisine(String cuisineItem) {
+    _cuisine ??= [];
+    _cuisine!.add(cuisineItem);
   }
 
-  addAvis(Avis avis){
-    this._avis ??= [];
-    this._avis?.add(avis);
-  }
-
-  List<double> get getCoordinates{
-    return [this._latitude, this._longitude];
-  }
-
-  double get getLatitude{
-    return this._latitude;
-  }
-
-  double get getLongitude{
-    return this._longitude;
-  }
-
-  List<String>? get getOpeningHours{
-    return this._openingHours;
-  }
-
-  String get getOsmId{
-    return this._osmId;
-  }
-
-  String get getName{
-    return this._name;
-  }
-
-  String get getType{
-    return this._type;
-  }
-
-  String? get getOperator{
-    return this._operator;
-  }
-
-  String? get getBrand{
-    return this._brand;
-  }
-
-  bool? get getWheelchair{
-    return this._wheelchair;
-  }
-
-  List<String>? get getCuisine{
-    return this._cuisine;
-  }
-
-  bool? get getVegetarian{
-    return this._vegetarian;
-  }
-
-  bool? get getVegan{
-    return this._vegan;
-  }
-
-  bool? get getDelivery{
-    return this._delivery;
-  }
-
-  bool? get getTakeaway{
-    return this._takeaway;
-  }
-
-  String? get getCapacity{
-    return this._capacity;
-  }
-
-  bool? get getDriveThrough{
-    return this._driveThrough;
-  }
-
-  String? get getphone{
-    return this._phone;
-  }
-
-  String? get getWebsite{
-    return this._website;
-  }
-
-  String? get getFacebook{
-    return this._facebook;
-  }
-
-  String get getRegion{
-    return this._region;
-  }
-
-  String get getDepartement{
-    return this._departement;
-  }
-
-  String get getCommune{
-    return this._commune;
+  @override
+  String toString() {
+    return 'Restaurant{name: $_name, type: $_type, region: $_region, departement: $_departement, commune: $_commune, cuisine: $_cuisine, vegetarian: $_vegetarian, vegan: $_vegan, delivery: $_delivery, takeaway: $_takeaway, phone: $_phone, website: $_website}';
   }
 }
