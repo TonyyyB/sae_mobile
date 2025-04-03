f3e2ad97c00b888d641ebc8dc073217aff9c0da7
varuniv <sargis.vardanyan@etu.univ-orelans.fr>
2025-04-03 16:12:53 +0200

    finalisation d'EtoileButtons

--- lib/src/widgets/etoile_buttons.dart
+++ lib/src/widgets/etoile_buttons.dart
@@ -3,72 +3,80 @@ import 'package:flutter/material.dart';
 
 import '../../config/colors.dart';
 
-const double _note = 0.0;
-
-const Icon star = Icon(color:PickMenuColors.iconsColor, IconData(0xe5f9, fontFamily: 'MaterialIcons'));
-const Icon star_empty = Icon(color:PickMenuColors.iconsColor,IconData(0xe5fa, fontFamily: 'MaterialIcons'));
-const Icon icon = star_empty;
+const Icon star = Icon(color: PickMenuColors.iconsColor, IconData(0xe5f9, fontFamily: 'MaterialIcons'));
+const Icon star_empty = Icon(color: PickMenuColors.iconsColor, IconData(0xe5fa, fontFamily: 'MaterialIcons'));
 
 class EtoileButtons extends StatefulWidget {
-  const EtoileButtons({super.key, this.note = _note});
-  final double note;
+  EtoileButtons({super.key, this.note = 0});
+  double note;
+
   @override
-  State<EtoileButtons> createState() => _EtoileButtonsState();
+  State<EtoileButtons> createState() => EtoileButtonsState();
+
+  double get getNote {
+    return this.note;
+  }
+
+  set setNote(double note) {
+    this.note = note;
+  }
 }
 
-class _EtoileButtonsState extends State<EtoileButtons> {
+class EtoileButtonsState extends State<EtoileButtons> {
   @override
   Widget build(BuildContext context) {
-    return Row(
+    Row bandeEtoile = Row(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
         IconButton(
-          icon: icon,
+          icon: widget.note >= 1 ? star : star_empty,
           tooltip: 'Note 1',
           onPressed: () {
             setState(() {
-              _note = 1;
+              widget.note = 1;
             });
           },
         ),
         IconButton(
-          icon: icon,
+          icon: widget.note >= 2 ? star : star_empty,
           tooltip: 'Note 2',
           onPressed: () {
             setState(() {
-              _note = 2;
+              widget.note = 2;
             });
           },
         ),
         IconButton(
-          icon: icon,
+          icon: widget.note >= 3 ? star : star_empty,
           tooltip: 'Note 3',
           onPressed: () {
             setState(() {
-              _note = 3;
+              widget.note = 3;
             });
           },
         ),
         IconButton(
-          icon: icon,
+          icon: widget.note >= 4 ? star : star_empty,
           tooltip: 'Note 4',
           onPressed: () {
             setState(() {
-              _note = 4;
+              widget.note = 4;
             });
           },
         ),
         IconButton(
-          icon: icon,
+          icon: widget.note >= 5 ? star : star_empty,
           tooltip: 'Note 5',
           onPressed: () {
             setState(() {
-              _note = 5;
+              widget.note = 5;
             });
           },
         ),
-        Text('Note : $_note'),
+        Text('Note : ${widget.note}'),
       ],
     );
+
+    return bandeEtoile;
   }
 }
